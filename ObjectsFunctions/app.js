8b94f082f1d1e5f19beacd00ec24589e7d4748bd
DavidLapadula <lapadula.david@gmail.com>
2022-04-11 22:22:17 -0400

    [objects-functions] quirks of 'this' keyword

--- ObjectsFunctions/app.js
+++ ObjectsFunctions/app.js
@@ -1,16 +1,59 @@
-var person = new Object(); 
+var person = new Object();
 
 person["firstname"] = "David"; // will create spot in memory, and person object will have reference to it
 person.lastname = "Smith";
 
 var firstNameProperty = "firstname";
 
-console.log(person);
-console.log(person[firstNameProperty]);
 
 var person2 = {
     name: "David",
     address: {
         number: "234 Ave"
+    }
+};
+
+// function statement
+function greet() {
+    console.log("Hi");
+}
+
+//function expression
+var anonymousGreet = function () {
+    console.log("Anonymous Hi");
+}
+
+// By value, so variables are made equal with 2 memory locations
+a = 3;
+b = a;
+
+// By reference, variables made equal by pointing to the same memory location
+c = { name: "David" };
+d = c;
+
+// Using non preexisting value, so new memory space made
+c = { name: "John" };
+
+// this
+function a() {
+    console.log(this); // will point to global object
+}
+
+var b = function() {
+    console.log(this); // will point to global object
+}
+
+var c = {
+    name : "The c object",
+    log: function() {
+        var self = this; 
+
+        console.log(this); // will point to the object
+        var setName = function (newname) {
+            this.name = newname; // 'this' points to global object
+            self.name = newname; // 'this' points to object
+        }
+
+        setName("New Name");
     }
 }
