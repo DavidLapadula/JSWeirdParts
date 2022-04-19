d7ebdaca7b81e63d0612c6bb56464bb7ce167ffa
DavidLapadula <lapadula.david@gmail.com>
2022-04-18 22:15:33 -0400

    [objects-functions] call(), apply(), bind()

--- ObjectsFunctions/closures.js
+++ ObjectsFunctions/closures.js
@@ -1,6 +1,6 @@
 function greet(whattosay) {
     return function (name) {
-        console.log(whattosay + ' ' + name); // whattosay avaiable because of scope chain, in outer ctx
+        // console.log(whattosay + ' ' + name); // whattosay avaiable because of scope chain, in outer ctx
     }
 }
 
@@ -17,7 +17,7 @@ function buildFunctions() {
     for (var i = 0; i < 3; i++) {
         arr.push(
             function () {
-                console.log(i); // not executed here, created object that is not invoked
+                // console.log(i); // not executed here, created object that is not invoked
             }
         )
     }
@@ -48,7 +48,7 @@ function buildFunctions2() {
             // j will stay in memoery even after function is invoked
             (function (j) {
                 return function () {
-                    console.log(j);
+                    // console.log(j);
                 }
             }(i))
         )
@@ -67,11 +67,11 @@ fs2[2]();
 function makeGreet(language) {
     return function (fname, lname) {
         if (language === 'en') {
-            console.log('Hello ' + fname + ' ' + lname);
+            // console.log('Hello ' + fname + ' ' + lname);
         }
 
         if (language === 'es') {
-            console.log('Hola ' + fname + ' ' + lname);
+            // console.log('Hola ' + fname + ' ' + lname);
         }
     }
 }
@@ -90,7 +90,7 @@ function sayHiLater() {
     // puts event on event loop, when timer is over, will find function to point to
     setTimeout(function(){
         // sayHiLater closes the value of greeting to ensure access
-        console.log(greeting);
+        // console.log(greeting);
     }, 3000);
 }
 
@@ -105,10 +105,91 @@ function tellMeWhenDone(callback) {
 }; 
 
 tellMeWhenDone(function(){
-    console.log('Done 1');
+    // console.log('Done 1');
 }); 
 
 
 tellMeWhenDone(function(){
-    console.log('Done 2');
+    // console.log('Done 2');
 }); 
+
+var person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname; 
+    }
+};
+
+var logName = function(lang1, lang2) {
+    // console.log('Logged: ' + this.getFullName());
+    // console.log('Argument: ' + lang1 + ', ' + lang2);
+    // console.log('-----------------------------');
+}; 
+
+var logPersonName = logName.bind(person);
+
+logPersonName('en');
+logName.call(person, 'fr');
+logName.apply(person, ['en', 'fr']);
+
+var person2 = {
+    firstname: 'Jane',
+    lastname: 'Doe',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname; 
+    }
+};
+
+// 'this' will refer to person2 (Borrowing)
+// console.log(person.getFullName.apply(person2));
+
+function multiply(a, b){
+    return a * b;
+}
+
+// this is lock in 2 as a the first parameter
+var multiplyByTwo = multiply.bind(this, 2);
+// console.log(multiplyByTwo(4));
+
+var arr1 = [1, 2, 3];
+console.log(arr1);
+
+function mapForEach(arr, fn){
+    var newArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        newArr.push(
+            fn(arr[i])
+        );
+    }
+
+    return newArr;
+}
+
+// function programming
+console.log(mapForEach(arr1, function(item) {
+    return item * 3; 
+}));
+console.log(mapForEach(arr1, item =>  item * 2));
+console.log(mapForEach(arr1, item =>  item > 2));
+
+var checkPastLimit = function(limiter, item) {
+    return item > limiter;
+}
+
+var checkPastOne = function(limiter) {
+    // just creating an object, limiter here is what is SET IN THE BIND
+    return function(limiter, item) {
+        return item > limiter;
+    }.bind(this, limiter);
+}
+
+var checkPastOne = function() {
+    return checkPastLimit.bind(this, 1)
+}
+
+// lock in 1 as the first paramter to checkPastLimit, second argument happens in map() fn
+// Need to invoke second argument because invocation itself returns an object with preset params
+var arr4 = mapForEach(arr1, checkPastOne());
+console.log(arr4);
